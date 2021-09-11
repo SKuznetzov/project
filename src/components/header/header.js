52d2ae7e6d2d5bb78771b7b7042ef384f6a50d7b
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-11 16:29:26 +0300

    создадим компонент Header

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+const Header = ()=>{
+  return (
+    <div className="header d-flex">
+      <h1>My Todo</h1>
+      
+    </div>
+  )
+}
+export default Header;
