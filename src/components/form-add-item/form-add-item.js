6d1457c95bdf1f519988986edb6610168225932f
SKuznetzov <s-kuznecov100@rambler.ru>
2021-09-16 10:22:47 +0300

    уберём Warning

--- src/components/form-add-item/form-add-item.js
+++ src/components/form-add-item/form-add-item.js
@@ -12,9 +12,9 @@ export default class FormAddItem extends Component {
   onSubmit=(e)=>{
       e.preventDefault()
       this.props.onItemAdd(this.state.label)
-      {this.setState({
+      this.setState({
           label: ''
-      })}
+      })
   }
     render() {
         return (
